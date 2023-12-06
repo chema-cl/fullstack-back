fc545c918fe8200a626d9d659a0c5ea17d415964
chema <josemanuel.carrasco.lopez@gmail.com>
2023-12-06 12:56:13 +0100

    Añade cometarios y correcciones de formato

--- actividad1.py
+++ actividad1.py
@@ -7,6 +7,15 @@ app = Flask('Actividad-01')
 
 @app.route('/pizza', methods=['POST'])
 def procesar_pedido():
+    """Procesa un pedido de pizza enviado mediante un formulario.
+
+    Esta función se activa cuando se realiza una solicitud POST a '/pizza'.
+    Obtiene los datos del formulario, como el nombre y apellidos del cliente,
+    los imprime en la consola y guarda el pedido utilizando el módulo 'persistencia'.
+
+    Returns:
+        flask.redirect: Redirecciona a la página 'solicita_pedido.html' del frontend.
+    """
     # Obtener los datos del formulario
     nombre = request.form.get('nombre')
     apellidos = request.form.get('apellidos')
@@ -16,12 +25,12 @@ def procesar_pedido():
     print("Apellidos:", apellidos)
 
     # Guardamos el pedido
-    persistencia.guardar_pedido(nombre,apellidos)   
+    persistencia.guardar_pedido(nombre,apellidos)
 
     # Redireccionar a la página 'solicita_pedido.html'
     return redirect(frontEndURLBase + "/solicita_pedido.html", code=302)
 
-# Control para que solo se levante el servidor de escucha si usamos este fichero 
+# Control para que solo se levante el servidor de escucha si usamos este fichero
 # como principal y no si lo estamos imporatando desde otro
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
--- persistencia.py
+++ persistencia.py
@@ -1,15 +1,41 @@
 def guardar_pedido(nombre, apellidos):
+    """Guarda un pedido en un archivo de texto.
+
+    Esta función recibe el nombre y apellidos de un cliente y los guarda
+    en un archivo de texto llamado 'pedidos.txt'.
+
+    Args:
+        nombre (str): El nombre del cliente.
+        apellidos (str): Los apellidos del cliente.
+
+    Returns:
+        None
+    """    
     with open('pedidos.txt', 'a', encoding='utf-8') as file:
         file.write(nombre + " " + apellidos + "\n")
         file.close()
 
 def leer_pedidos():
+    """Lee y muestra todos los pedidos guardados en el archivo 'pedidos.txt'.
+
+    Lee el archivo 'pedidos.txt' línea por línea y muestra cada pedido en la consola.
+
+    Returns:
+        None
+    """
     with open('pedidos.txt', 'r', encoding='utf-8') as file:
         for pedido in file:
             print(pedido.strip())
         file.close()
 
 def limpiar_pedidos():
+    """Limpia el contenido del archivo 'pedidos.txt'.
+
+    Esta función borra todo el contenido del archivo 'pedidos.txt'.
+
+    Returns:
+        None
+    """
     with open('pedidos.txt', 'w', encoding='utf-8') as file:
         file.write("")
-        file.close()    
+        file.close()
